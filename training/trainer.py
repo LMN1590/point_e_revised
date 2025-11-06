806442fde88bceefc20e22d3b85761f92b2bb46e
Nghia Le <nghia.le@novobi.vn>
2025-11-06 14:55:12 +0700

    testing loss function for gaussian

--- training/trainer.py
+++ training/trainer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from pytorch_lightning import LightningModule
 
-from typing import Any
+from typing import Any,Dict
 import typing
 import os
 
@@ -15,7 +15,7 @@ class DiffusionTrainer(LightningModule):
     def __init__(
         self,
         noise_pred_net:GripperRepDiffusionTransformer,
-        diffusion_scheduler:GaussianDiffusion,
+        diffusion:GaussianDiffusion,
         
         ema_power:float = 0.75,
         ema_update_after_step:int = 0,
@@ -44,6 +44,7 @@ class DiffusionTrainer(LightningModule):
             power=ema_power,
             update_after_step=ema_update_after_step,
         )
+        self.diffusion = diffusion
         
         self.learning_rate= learning_rate
         self.lr_warmup_steps = lr_warmup_steps
@@ -55,11 +56,43 @@ class DiffusionTrainer(LightningModule):
         return typing.cast(
             GripperRepDiffusionTransformer, self.ema_nets.get_submodule("noise_pred_net")
         )
-        
     
-    def training_step(self,gripper_embeddings:torch.Tensor,batch_idx):
+    def training_step(self,gripper_data:Dict[str,torch.Tensor],batch_idx:int):
+        """
+        Args:
+            gripper_data (Dict[str,torch.Tensor]): 2 keys 'grippers' - [B,sample,gripper_dim_mask,finger*segments] and 'object_embeddings' - [B,1,feature_dim,n_ctx]
+            batch_idx (int): Index of current batch
+        """
+        B,S = gripper_data['grippers'].shape[:2]
+        grippers = gripper_data['grippers'].flatten(0,1) # [B*sample_size,gripper_dim_mask, finger*segments ~ n_ctx]
+        object_encoding = gripper_data['object_embedding'].flatten(0,1) # [B*sample,obj_dim,n_ctx_obj]
+        
+        timesteps = torch.randint(
+            0,
+            self.diffusion.num_timesteps,  # type: ignore
+            (B * S,),
+            device=self.device,
+        ).long()
+        
+        loss_dict = self.diffusion.training_losses(
+            model=self.noise_pred_net,
+            x_start = grippers,
+            t = timesteps,
+            model_kwargs = {
+                "embeddings": object_encoding
+            }
+        )
+        breakpoint()
+        
+        
         return None
-    def validation_step(self,gripper_embeddings:torch.Tensor,batch_idx):
+    def validation_step(self,gripper_data:Dict[str,torch.Tensor],batch_idx:int):
+        """
+        Args:
+            gripper_data (Dict[str,torch.Tensor]): 2 keys 'grippers' - [B,sample,gripper_dim_mask,finger*segments] and 'object_embeddings' - [B,1,feature_dim,n_ctx]
+            batch_idx (int): Index of current batch
+            
+        """
         return None
     
     def configure_optimizers(self):
