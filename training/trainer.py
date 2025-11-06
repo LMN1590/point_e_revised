b002196ec344be564c843be02520a8231691c255
LMN1590 <minhnghiale1590@gmail.com>
2025-11-06 10:13:49 +0700

    sync

--- training/trainer.py
+++ training/trainer.py
@@ -1,5 +1,10 @@
+from typing import Any
 from pytorch_lightning import LightningModule
 
 class DiffusionTrainer(LightningModule):
     def __init__(self):
         pass
+    def training_step(self, *args: Any, **kwargs: Any) -> Tensor | Mapping[str, Any] | None:
+        return super().training_step(*args, **kwargs)
+    def validation_step(self, *args: Any, **kwargs: Any) -> Tensor | Mapping[str, Any] | None:
+        return super().validation_step(*args, **kwargs)
